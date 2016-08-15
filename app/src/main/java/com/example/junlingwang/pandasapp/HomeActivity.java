4fa51747ab889e90f49c2746cd1de3279ec8408d
Junling Wang <junling-wang@hotmail.com>
2016-08-15 22:24:36 +1000

    1

--- app/src/main/java/com/example/junlingwang/pandasapp/HomeActivity.java
+++ app/src/main/java/com/example/junlingwang/pandasapp/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.junlingwang.pandasapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void go_to_order (View view) {
+        Intent go_to = new Intent(this, OrderActivity.class);
+        startActivity(go_to);
+    }
 }
+
