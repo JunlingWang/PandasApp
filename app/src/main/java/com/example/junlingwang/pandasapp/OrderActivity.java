ec90ddf700211b4847c9057bf1c7e9b86ac977c7
Junling Wang <junling-wang@hotmail.com>
2016-08-15 22:45:59 +1000

    1

--- app/src/main/java/com/example/junlingwang/pandasapp/OrderActivity.java
+++ app/src/main/java/com/example/junlingwang/pandasapp/OrderActivity.java
@@ -2,6 +2,7 @@ package com.example.junlingwang.pandasapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        WebView webView = (WebView) findViewById(R.id.orderWebView);
+        webView.loadUrl("http://www.pandatownsville.com/menu/");
     }
 }
